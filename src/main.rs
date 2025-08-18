f40e5b219180af32530820696700f04f702d351f
Tiago Martins <tiago.sm99@gmail.com>
2025-08-18 20:09:53 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
 #![allow(dead_code)]
-pub mod fem {
-    pub mod assembly;
 
-    pub mod elements {
-        pub mod parametric_topology_element {
-            pub mod elastic_force_matrices {
-                pub mod parametric_expansion_with_recursion;
-            }
-            pub mod determinant_and_adjugate;
-            pub mod position_jacobian;
-            //pub mod integrate_elements;
-            //pub mod mass_matrices;
+pub mod assembly;
+
+pub mod elements {
+    pub mod parametric_topology_element {
+        pub mod elastic_force_matrices {
+            pub mod parametric_expansion_with_recursion;
         }
-        pub mod quadrature {
-            pub mod quadrature_rules;
-        }
-        pub mod element_library {
-            pub mod hypercube_elements;
-        }
+        pub mod determinant_and_adjugate;
+        pub mod position_jacobian;
+        //pub mod integrate_elements;
+        //pub mod mass_matrices;
     }
-    pub mod read_mesh {
-        pub mod locate_nodes_o_log_n;
+    pub mod quadrature {
+        pub mod quadrature_rules;
+    }
+    pub mod element_library {
+        pub mod hypercube_elements;
     }
 }
+
+pub mod read_mesh {
+    pub mod locate_nodes_o_log_n;
+}
+
 
 fn main() { 
     println!("Hello, world!");
